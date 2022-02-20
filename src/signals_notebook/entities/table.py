090f10147dc5d6b389bf7ddf555027ab3cdff2d1
Evgeniy Pokidov <evgeniy.pokidov@quantori.com>
2022-02-20 20:29:24 +0300

    Ability to index over rows and cells

--- src/signals_notebook/entities/table.py
+++ src/signals_notebook/entities/table.py
@@ -1,11 +1,10 @@
-from collections import defaultdict
 from enum import Enum
 from operator import attrgetter
-from typing import Any, Dict, List, Literal, Optional
+from typing import Any, Dict, List, Literal, Optional, Union
 from uuid import UUID
 
 import pandas as pd
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr
 
 from signals_notebook.api import SignalsNotebookApi
 from signals_notebook.entities.contentful_entity import ContentfulEntity
@@ -60,7 +59,7 @@ class Cell(BaseModel):
         validate_assignment = True
 
     @property
-    def value(self) -> Any :
+    def value(self) -> Any:
         return self.content.value
 
 
@@ -68,13 +67,32 @@ class Row(BaseModel):
     id: UUID = Field(allow_mutation=False)
     type: Literal[EntityType.ADT_ROW] = Field(allow_mutation=False)
     cells: List[Cell]
+    _cells_by_key: Dict[UUID, Cell] = PrivateAttr(default={})
 
     class Config:
         validate_assignment = True
 
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        for cell in self.cells:
+            self._cells_by_key[cell.key] = cell
+
     def get_values(self, use_labels: bool = True) -> Dict[str, Any]:
         key_getter = attrgetter('name') if use_labels else attrgetter('key')
         return {key_getter(cell): cell.value for cell in self.cells}
+
+    def __getitem__(self, index: Union[int, str, UUID]) -> Cell:
+        if isinstance(index, int):
+            return self.cells[index]
+
+        if isinstance(index, str):
+            return self._cells_by_key[UUID(index)]
+
+        if isinstance(index, UUID):
+            return self._cells_by_key[index]
+
+        raise IndexError('Invalid index type')
 
 
 class TableDataResponse(Response[Row]):
@@ -87,6 +105,8 @@ class ColumnDefinitionsResponse(Response[ColumnDefinitions]):
 
 class Table(ContentfulEntity):
     type: Literal[EntitySubtype.GRID] = Field(allow_mutation=False)
+    _rows: Optional[List[Row]] = PrivateAttr(default=None)
+    _rows_by_id: Optional[Dict[UUID, Row]] = PrivateAttr(default=None)
 
     @classmethod
     def _get_subtype(cls) -> EntitySubtype:
@@ -96,7 +116,7 @@ class Table(ContentfulEntity):
     def _get_adt_endpoint(cls) -> str:
         return 'adt'
 
-    def _get_data(self) -> TableDataResponse:
+    def _reload_data(self) -> None:
         api = SignalsNotebookApi.get_default_api()
 
         response = api.call(
@@ -104,7 +124,14 @@ class Table(ContentfulEntity):
             path=(self._get_adt_endpoint(), self.eid),
         )
 
-        return TableDataResponse(**response.json())
+        result = TableDataResponse(**response.json())
+
+        self._rows = []
+        self._rows_by_id = {}
+        for item in result.data:
+            row = item.body
+            self._rows.append(row)
+            self._rows_by_id[row.id] = row
 
     def get_column_definitions(self) -> List[ColumnDefinition]:
         api = SignalsNotebookApi.get_default_api()
@@ -118,24 +145,42 @@ class Table(ContentfulEntity):
 
         return result.data.body.columns
 
-    def as_dataframe(self, use_labels: bool=True) -> pd.DataFrame:
-        result = self._get_data()
+    def as_dataframe(self, use_labels: bool = True) -> pd.DataFrame:
+        if self._rows is None:
+            self._reload_data()
 
         data = []
         index = []
-        for response_data in result.data:
-            row = response_data.body
+        for row in self._rows:
             index.append(row.id)
             data.append(row.get_values(use_labels))
 
         return pd.DataFrame(data=data, index=index)
 
-    def as_rows(self, use_labels: bool=True) -> List[Dict[str, Any]]:
-        result = self._get_data()
+    def as_raw_data(self, use_labels: bool = True) -> List[Dict[str, Any]]:
+        if self._rows is None:
+            self._reload_data()
 
         data = []
-        for response_data in result.data:
-            row = response_data.body
+        for row in self._rows:
             data.append(row.get_values(use_labels))
 
         return data
+
+    def __getitem__(self, index: Union[int, str, UUID]) -> Row:
+        if self._rows is None:
+            self._reload_data()
+
+        if isinstance(index, int):
+            return self._rows[index]
+
+        if isinstance(index, str):
+            return self._rows_by_id[UUID(index)]
+
+        if isinstance(index, UUID):
+            return self._rows_by_id[index]
+
+        raise IndexError('Invalid index type')
+
+    def __iter__(self):
+        return self._rows.__iter__()
