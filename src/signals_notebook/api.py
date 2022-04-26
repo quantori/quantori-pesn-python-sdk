8166d1c3028ef26964ccd7ffbc708765fe6eb092
Evgeniy Pokidov <evgeniy.pokidov@quantori.com>
2022-04-26 16:34:04 +0400

    PSFSN-13 Add notebook with Notebook entity

--- src/signals_notebook/api.py
+++ src/signals_notebook/api.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any, Dict, IO, Iterable, Mapping, Optional, Sequence, Tuple, Union
 
 import requests
@@ -22,10 +23,10 @@ class SignalsNotebookApi:
         self._session = session
 
     @classmethod
-    def init(cls, api_host: str, api_key: str) -> 'SignalsNotebookApi':
-        cls._api_host = api_host
+    def init(cls, api_host: Optional[str] = None, api_key: Optional[str] = None) -> 'SignalsNotebookApi':
+        cls._api_host = api_host if api_host else os.getenv('SN_API_HOST', '')
         session = requests.Session()
-        session.headers.update({'x-api-key': api_key})
+        session.headers.update({'x-api-key': api_key if api_key else os.getenv('SN_API_KEY', '')})
 
         api = cls(session)
         cls.set_default_api(api)
