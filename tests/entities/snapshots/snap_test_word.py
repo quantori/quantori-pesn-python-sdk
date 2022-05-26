b21922522dcaca58b4d1a5aae6606b1d75215ca9
Daniil <daniil.cherednichenko@quantori.com>
2022-05-26 13:42:09 +0300

    PSFSN-18 Convert generated HTML to PDF: added right naming for downloaded file

--- tests/entities/snapshots/snap_test_word.py
+++ tests/entities/snapshots/snap_test_word.py
@@ -10,6 +10,8 @@ snapshots = Snapshot()
 snapshots['test_get_html 1'] = '''<div style="margin: 10px 10px; padding: 6px 10px; border: solid 1px #f5f5f5;">
     <h2>name</h2>
     
-    <a href="data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,U29tZSB0ZXh0" download="file.docx">DOWNLOAD .DOCX FILE</a>
+    
+    <a href="data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,U29tZSB0ZXh0" download="name.docx">DOWNLOAD .DOCX FILE</a>
+    
     
 </div>'''
