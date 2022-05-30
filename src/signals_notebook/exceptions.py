fa862d0b640c39db3ac4e8ac99633da47018dd6d
andrey.shishkin <andrey.shishkin@quantori.com>
2022-05-30 18:29:24 +0300

    PSFSN-19. add docstring in exceptions.py

--- src/signals_notebook/exceptions.py
+++ src/signals_notebook/exceptions.py
@@ -16,10 +16,23 @@ class ErrorResponse(BaseModel):
 
 
 class SignalsNotebookError(Exception):
+    """Handle Signals Notebook API errors
+
+    """
     def __init__(self, response: requests.Response):
+        """
+
+        Args:
+            response: Response object, which contains a server's response to an HTTP request.
+        """
         self.parsed_response = parse_obj_as(ErrorResponse, response.json())
 
     def __str__(self) -> str:
+        """Get error in string format
+
+        Returns:
+            Error with details
+        """
         error = self.parsed_response.errors[0]
         return (
             f'<SignalsNotebookError status={error.status} code={error.code}> title={error.title} detail={error.detail}>'
