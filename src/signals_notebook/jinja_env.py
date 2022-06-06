90896ebd9f92ed6b43fc6c9f5d9e3535a0134e7c
Daniil <daniil.cherednichenko@quantori.com>
2022-06-06 12:49:06 +0300

    PSFSN-21: added extra try-except in get_template method/ added type hints

--- src/signals_notebook/jinja_env.py
+++ src/signals_notebook/jinja_env.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape, TemplateNotFound
+from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape, TemplateNotFound, Template
 
 package_env = Environment(loader=PackageLoader('signals_notebook'), autoescape=select_autoescape())
 
@@ -10,14 +10,19 @@ log = logging.getLogger(__name__)
 
 class TemplateLocationWrapper:
 
-    def get_template(self, template_name):
+    def get_template(self, template_name: str) -> Template:
         dir_path, file_name = os.path.split(template_name)
         try:
             file_system_env = Environment(loader=FileSystemLoader(os.path.abspath(dir_path)))
             return file_system_env.get_template(file_name)
         except TemplateNotFound:
-            log.exception('There is no template in the system. Searching in package location...')
+            log.info('There is no template in the system. Searching in package location...')
+
+        try:
             return package_env.get_template(file_name)
+        except TemplateNotFound:
+            log.info('There is no template in the package.')
+            raise
 
 
 env = TemplateLocationWrapper()
