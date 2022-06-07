db3e89344d0917746da2a6373596990380686f35
Daniil <daniil.cherednichenko@quantori.com>
2022-06-07 17:00:18 +0300

    PSFSN-24: added extra properties to CellPropertyContent

--- src/signals_notebook/entities/samples/cell.py
+++ src/signals_notebook/entities/samples/cell.py
@@ -1,11 +1,13 @@
 from typing import TypeVar, Generic, Optional
 from pydantic.generics import GenericModel
 
-from signals_notebook.common_types import EntityType
+from signals_notebook.common_types import EntityType, EID
 
 CellContentType = TypeVar('CellContentType')
 
 
 class CellPropertyContent(GenericModel, Generic[CellContentType]):
-    value: CellContentType
+    value: Optional[CellContentType]
     type: Optional[EntityType] = None
+    eid: Optional[EID]
+    name: Optional[str]
