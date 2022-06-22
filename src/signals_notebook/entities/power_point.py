f397b0f26671ad0495cb6590fa3b82d1a593d4cc
andrey.shishkin <andrey.shishkin@quantori.com>
2022-06-22 12:57:30 +0300

    SBT-8. added new entity PowerPoint

---
+++ src/signals_notebook/entities/power_point.py
@@ -0,0 +1,33 @@
+import logging
+from typing import ClassVar, Literal
+
+from pydantic import Field
+
+from signals_notebook.common_types import EntityType, File
+from signals_notebook.entities import Entity
+from signals_notebook.entities.container import Container
+from signals_notebook.entities.contentful_entity import ContentfulEntity
+
+log = logging.getLogger(__name__)
+
+
+class PowerPoint(ContentfulEntity):
+    type: Literal[EntityType.POWER_POINT] = Field(allow_mutation=False)
+    _template_name: ClassVar = 'power_point.html'
+
+    @classmethod
+    def _get_entity_type(cls) -> EntityType:
+        return EntityType.POWER_POINT
+
+    @classmethod
+    def create(cls, *, container: Container, name: str, content: str = '', force: bool = True) -> Entity:
+        log.debug('Create entity: %s with name: %s in Container: %s', cls.__name__, name, container.eid)
+        return container.add_child(
+            name=name,
+            content=content.encode('utf-8'),
+            content_type='application/vnd.openxmlformats-officedocument.presentationml.presentation',
+            force=force,
+        )
+
+    def get_content(self) -> File:
+        return super()._get_content()
