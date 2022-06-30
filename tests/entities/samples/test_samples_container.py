896b1bee56653a4ae16aef40c7ec00b09c44b18d
andrey.shishkin <andrey.shishkin@quantori.com>
2022-06-30 17:24:34 +0300

    SBT-15. corrected tests

--- tests/entities/samples/test_samples_container.py
+++ tests/entities/samples/test_samples_container.py
@@ -91,6 +91,7 @@ def test_get_content(samples_container_factory, api_mock, samples_container_cont
     api_mock.call.return_value.headers = {
         'content-type': content_type,
         'content-disposition': f'attachment; filename={file_name}',
+        'content-length': 1,
     }
     api_mock.call.return_value.content = content
 
