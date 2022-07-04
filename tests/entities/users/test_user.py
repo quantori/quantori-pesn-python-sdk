943b9882079631d8439281f8d115d083907660dc
andrey.shishkin <andrey.shishkin@quantori.com>
2022-07-04 16:28:28 +0300

    SBT-23. finished tests tp user

--- tests/entities/users/test_user.py
+++ tests/entities/users/test_user.py
@@ -1,6 +1,7 @@
 import arrow
 import pytest
 
+from signals_notebook.common_types import File
 from signals_notebook.entities.users.user import User, UserCreationBody
 from signals_notebook.entities.users.user_store import UserStore
 from signals_notebook.entities.users.profile import Profile
@@ -56,3 +57,65 @@ def test_create(api_mock):
     assert result.first_name == response['data']['attributes']['firstName']
     assert result.last_name == response['data']['attributes']['lastName']
     assert result.organization == response['data']['attributes']['organization']
+
+
+def test_save(api_mock, user_factory):
+    user = user_factory()
+    new_first_name = 'Update first name'
+    assert user.first_name != new_first_name
+
+    user.first_name = new_first_name
+
+    user.save()
+
+    api_mock.call.assert_called_once_with(
+        method='PATCH',
+        path=('users', user.id),
+        json={
+            "data": {
+                "attributes": {
+                    "alias": user.alias,
+                    "country": user.country,
+                    "firstName": 'Update first name',
+                    "lastName": user.last_name,
+                    "organization": user.organization,
+                },
+            },
+        },
+    )
+    assert user.first_name == new_first_name
+
+
+def test_delete(api_mock, user_factory):
+    user = user_factory()
+    user.delete()
+
+    api_mock.call.assert_called_once_with(
+        method='DELETE',
+        path=('users', user.id),
+    )
+
+
+def test_picture(api_mock, user_factory):
+    content_type = 'image/jpeg'
+    content = b'image'
+
+    user = user_factory()
+    file_name = f'{user.first_name}_{user.last_name}.{content_type.split("/")[-1]}'
+
+    api_mock.call.return_value.content = content
+    api_mock.call.return_value.headers = {
+        'content-type': content_type,
+        'content-disposition': f'attachment; filename={file_name}',
+    }
+    result = user.picture
+
+    api_mock.call.assert_called_once_with(
+        method='GET',
+        path=('users', user.id, 'picture'),
+    )
+
+    assert isinstance(result, File)
+    assert result.name == file_name
+    assert result.content == content
+    assert result.content_type == content_type
