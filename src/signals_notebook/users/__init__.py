20f36d46eb7f07e23b82f20019fba36d6922377a
andrey.shishkin <andrey.shishkin@quantori.com>
2022-07-12 11:21:24 +0300

    SBT-24. group user imports

--- src/signals_notebook/users/__init__.py
+++ src/signals_notebook/users/__init__.py
@@ -1,7 +1,4 @@
 from signals_notebook.users.user import User  # noqa
-from signals_notebook.users.user_store import UserStore # noqa
-from signals_notebook.users.profile import Profile  # noqa
-from signals_notebook.users.profile import Licence  # noqa
-from signals_notebook.users.profile import Role  # noqa
+from signals_notebook.users.user_store import UserStore  # noqa
+from signals_notebook.users.profile import Licence, Privelege, Profile, Role  #  # noqa
 from signals_notebook.users.group import Group  # noqa
-from signals_notebook.users.profile import Privelege  # noqa
