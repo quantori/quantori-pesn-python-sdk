c72e6c1904b15c7c33eea2b6af318a444f002a0a
Daniil <daniil.cherednichenko@quantori.com>
2022-07-14 14:41:33 +0300

    SBT-46 Implement dump/load for PowerPoint

--- src/signals_notebook/entities/power_point.py
+++ src/signals_notebook/entities/power_point.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import ClassVar, Literal
 
@@ -7,6 +8,7 @@ from signals_notebook.common_types import EntityType, File
 from signals_notebook.entities import Entity
 from signals_notebook.entities.container import Container
 from signals_notebook.entities.contentful_entity import ContentfulEntity
+from signals_notebook.utils import FSHandler
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +22,7 @@ class PowerPoint(ContentfulEntity):
         return EntityType.POWER_POINT
 
     @classmethod
-    def create(cls, *, container: Container, name: str, content: str = '', force: bool = True) -> Entity:
+    def create(cls, *, container: Container, name: str, content: bytes = b'', force: bool = True) -> Entity:
         """Create PowerPoint Entity
 
         Args:
@@ -35,7 +37,7 @@ class PowerPoint(ContentfulEntity):
         log.debug('Create entity: %s with name: %s in Container: %s', cls.__name__, name, container.eid)
         return container.add_child(
             name=name,
-            content=content.encode('utf-8'),
+            content=content,
             content_type='application/vnd.openxmlformats-officedocument.presentationml.presentation',
             force=force,
         )
@@ -47,3 +49,41 @@ class PowerPoint(ContentfulEntity):
             File
         """
         return super()._get_content()
+
+    def dump(self, base_path: str, fs_handler: FSHandler) -> None:
+        """Dump Word entity
+
+        Args:
+            base_path: content path where create dump
+            fs_handler: FSHandler
+
+        Returns:
+
+        """
+        content = self.get_content()
+        metadata = {
+            'file_name': content.name,
+            **{k: v for k, v in self.dict().items() if k in ('name', 'description', 'eid')},
+        }
+        fs_handler.write(fs_handler.join_path(base_path, self.eid, 'metadata.json'), json.dumps(metadata))
+        file_name = content.name
+        data = content.content
+        fs_handler.write(fs_handler.join_path(base_path, self.eid, file_name), data)
+
+    @classmethod
+    def load(cls, path: str, fs_handler: FSHandler, parent: Container) -> None:
+        """Load Word entity
+
+        Args:
+            path: content path
+            fs_handler: FSHandler
+            parent: Container where load Word entity
+
+        Returns:
+
+        """
+        metadata_path = fs_handler.join_path(path, 'metadata.json')
+        metadata = json.loads(fs_handler.read(metadata_path))
+        content_path = fs_handler.join_path(path, metadata['file_name'])
+        content = fs_handler.read(content_path)
+        cls.create(container=parent, name=metadata['name'], content=content, force=True)
