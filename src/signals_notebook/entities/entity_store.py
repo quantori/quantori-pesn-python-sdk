152e5146e2956f208e074c342039e51a3d706422
Daniil <daniil.cherednichenko@quantori.com>
2022-08-03 14:57:41 +0300

    SBT-71 Samples dumping templates: fixed flake8 problems

--- src/signals_notebook/entities/entity_store.py
+++ src/signals_notebook/entities/entity_store.py
@@ -170,7 +170,6 @@ class EntityStore:
         Returns:
 
         """
-        from signals_notebook.entities import Experiment, Table
 
         for item in Entity.get_subclasses():
             item.dump_templates(base_path, fs_handler)
