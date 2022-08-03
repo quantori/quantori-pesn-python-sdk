188290e234751e96bcaa80e708b0d0b444fd1603
andrey.shishkin <andrey.shishkin@quantori.com>
2022-08-03 07:50:59 +0300

    SBT-62. add row and cell to subexpsummary

--- src/signals_notebook/entities/parallel_experiment/sub_experiment_summary.py
+++ src/signals_notebook/entities/parallel_experiment/sub_experiment_summary.py
@@ -1,18 +1,25 @@
 import logging
-from typing import cast, Literal, Union
-
-from pydantic import Field
+from typing import cast, Literal, Union, List, Dict
+from uuid import UUID
+from pydantic import Field, PrivateAttr
 
 from signals_notebook.api import SignalsNotebookApi
 from signals_notebook.common_types import EntityType, File, ResponseData, Response
 from signals_notebook.entities.contentful_entity import ContentfulEntity
+from signals_notebook.entities.parallel_experiment.row import Row
 
 log = logging.getLogger(__name__)
 
 
+class SubExpSummaryResponse(Response[Row]):
+    pass
+
+
 class SubExperimentSummary(ContentfulEntity):
     type: Literal[EntityType.SUB_EXPERIMENT_SUMMARY] = Field(allow_mutation=False)
-    # _template_name: ClassVar = 'excel.html'
+    _rows: List[Row] = PrivateAttr(default=[])
+    _rows_by_id: Dict[Union[str, UUID], Row] = PrivateAttr(default={})
+    _template_name = 'subexp_summary.html'
 
     @classmethod
     def _get_entity_type(cls) -> EntityType:
@@ -22,32 +29,71 @@ class SubExperimentSummary(ContentfulEntity):
     def _get_endpoint(cls) -> str:
         return 'subexpSummary'
 
-    # def get_content(self) -> File:
-    #     """Get Excel content
-    #
-    #     Returns:
-    #         File
-    #     """
-    #     return super()._get_content()
+    def __getitem__(self, index: Union[int, str, UUID]) -> Row:
+        if not self._rows:
+            self._reload_cells()
 
-    def get_rows(self):
+        if isinstance(index, int):
+            return self._rows[index]
+
+        if isinstance(index, str):
+            try:
+                return self._rows_by_id[UUID(index)]
+            except ValueError:
+                return self._rows_by_id[index]
+
+        if isinstance(index, UUID):
+            return self._rows_by_id[index]
+
+        raise IndexError('Invalid index')
+
+    def __iter__(self):
+        if not self._rows:
+            self._reload_cells()
+        return self._rows.__iter__()
+
+    def _reload_cells(self):
         api = SignalsNotebookApi.get_default_api()
-        # log.debug('Create Entity: %s...', cls.__name__)
+        log.debug('Reloading data in Table: %s...', self.eid)
 
         response = api.call(
             method='GET',
             path=(self._get_endpoint(), self.eid, 'rows'),
         )
-        # log.debug('Entity: %s was created.', cls.__name__)
 
-        result = Response(**response.json())  # type: ignore
-        yield from [cast(ResponseData, item).body for item in result.data]
+        result = SubExpSummaryResponse(**response.json())  # type: ignore
+        cells = [cast(ResponseData, item).body for item in result.data]
 
-        while result.links and result.links.next:
-            response = api.call(
-                method='GET',
-                path=result.links.next,
-            )
+        self._rows = []
+        self._rows_by_id = {}
 
-            result = Response(**response.json())  # type: ignore
-            yield from [cast(ResponseData, item).body for item in result.data]
+        for item in cells:
+            subexp_row = cast(Row, item)
+            assert subexp_row.id
+
+            self._rows.append(subexp_row)
+            self._rows_by_id[subexp_row.id] = subexp_row
+
+    # def save(self, force: bool = True) -> None:
+    #     api = SignalsNotebookApi.get_default_api()
+    #
+    #     request_body = []
+    #     for item in self._cells:
+    #         if item.is_changed:
+    #             request_body.append(item.representation_for_update.dict(exclude_none=True))
+    #
+    #     if not request_body:
+    #         return
+    #
+    #     api.call(
+    #         method='PATCH',
+    #         path=(self._get_samples_endpoint(), self.eid, 'properties'),
+    #         params={
+    #             'force': json.dumps(force),
+    #             'value': 'normalized',
+    #         },
+    #         json={
+    #             'data': {'attributes': {'data': request_body}},
+    #         },
+    #     )
+    #     self._reload_cells()
