26ad636c9283281817cc3a67196807fb7dc6500d
Daniil <daniil.cherednichenko@quantori.com>
2022-08-09 13:11:35 +0300

    SBT-70 Tasks and Requests templates + tests: added test for create

--- tests/entities/test_request.py
+++ tests/entities/test_request.py
@@ -5,7 +5,7 @@ import arrow
 import pytest
 
 from signals_notebook.common_types import EntityType, ObjectType
-from signals_notebook.entities import ChemicalDrawing, Entity, Text
+from signals_notebook.entities import ChemicalDrawing, Entity, Text, RequestContainer
 
 
 @pytest.fixture()
@@ -39,8 +39,60 @@ def templates():
     }
 
 
-def test_create():
-    pass
+@pytest.mark.parametrize('description', ['test description', None])
+@pytest.mark.parametrize('digest, force', [('1234234', False), (None, True)])
+def test_create(api_mock, description, digest, force, eid_factory):
+    eid = eid_factory(type=EntityType.REQUEST)
+    response = {
+        'links': {'self': f'https://example.com/{eid}'},
+        'data': {
+            'type': ObjectType.ENTITY,
+            'id': eid,
+            'links': {'self': f'https://example.com/{eid}'},
+            'attributes': {
+                'eid': eid,
+                'name': 'My request',
+                'description': description,
+                'type': EntityType.REQUEST,
+                'createdAt': '2019-09-06T03:12:35.129Z',
+                'editedAt': '2019-09-06T15:22:47.309Z',
+                'digest': digest,
+            },
+        },
+    }
+    api_mock.call.return_value.json.return_value = response
+
+    result = RequestContainer.create(name='My request', description=description, digest=digest, force=force)
+
+    request_body = {
+        'data': {
+            'type': EntityType.REQUEST,
+            'attributes': {
+                'name': response['data']['attributes']['name'],
+            },
+        }
+    }
+
+    if description:
+        request_body['data']['attributes']['description'] = description
+
+    api_mock.call.assert_called_once_with(
+        method='POST',
+        path=('entities',),
+        params={
+            'digest': digest,
+            'force': 'true' if force else 'false',
+        },
+        json=request_body,
+    )
+
+    assert isinstance(result, RequestContainer)
+    assert result.eid == eid
+    assert result.digest == response['data']['attributes']['digest']
+    assert result.name == response['data']['attributes']['name']
+    assert result.description == response['data']['attributes']['description']
+    assert result.created_at == arrow.get(response['data']['attributes']['createdAt'])
+    assert result.edited_at == arrow.get(response['data']['attributes']['editedAt'])
 
 
 def test_create_with_ancestors():
