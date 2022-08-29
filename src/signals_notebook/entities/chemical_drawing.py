0aba4c41d53f182a4b8ae2b1c866c80ddf1a284c
andrey.shishkin <andrey.shishkin@quantori.com>
2022-08-29 14:42:35 +0300

    corrected naming CreationContentType

--- src/signals_notebook/entities/chemical_drawing.py
+++ src/signals_notebook/entities/chemical_drawing.py
@@ -28,7 +28,7 @@ class ChemicalDrawing(ContentfulEntity):
         CSV = 'text/csv'
         SMILES = 'chemical/x-daylight-smiles'
 
-    class CreateChemDrawContentType(str, Enum):
+    class CreationContentType(str, Enum):
         CDX = 'chemical/x-cdx'
         CDXML = 'chemical/x-cdxml'
         MOL = 'chemical/x-mdl-molfile'
@@ -49,7 +49,7 @@ class ChemicalDrawing(ContentfulEntity):
         *,
         container: Container,
         name: str,
-        content_type: str = ContentType.CDXML,
+        content_type: str = CreationContentType.CDXML,
         content: bytes = b'',
         force: bool = True,
     ) -> Entity:
@@ -66,7 +66,7 @@ class ChemicalDrawing(ContentfulEntity):
             ChemicalDrawing
         """
         if content_type:
-            cls.CreateChemDrawContentType(content_type)
+            cls.CreationContentType(content_type)
         log.debug('Create entity: %s with name: %s in Container: %s', cls.__name__, name, container.eid)
         return container.add_child(
             name=name,
