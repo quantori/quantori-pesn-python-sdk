7f369ca5ffdc9c080b2a86f497e376eaabfee3a3
Andrey Guschin <aguschin@thrivedetect.com>
2022-09-07 02:33:19 +0300

    Fix restore

--- src/signals_notebook/entities/notebook.py
+++ src/signals_notebook/entities/notebook.py
@@ -72,7 +72,8 @@ class Notebook(Container):
         )
 
     def dump(self, base_path: str, fs_handler: FSHandler, alias: Optional[Tuple[str]]) -> None:
-        metadata = {k: v for k, v in self.dict().items() if k in ('name', 'description', 'eid')},
+        metadata = {k: v for k, v in self.dict().items() if k in ('name', 'description', 'eid')}
+        print('dump', self.dict().items())
         metadata['organization'] = self['organization']
         fs_handler.write(
             fs_handler.join_path(base_path, self.eid, 'metadata.json'),
